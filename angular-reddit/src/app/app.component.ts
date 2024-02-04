24e6dddfa5694c6740955e78b0edb2cc9f8fcc0b
Josh McHugh <joshua.mchugh01@gmail.com>
2024-02-03 21:44:10 -0500

    Added Article model, form ability to add article, and sort by most upvoted

--- angular-reddit/src/app/app.component.ts
+++ angular-reddit/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ArticleComponent } from './article/article.component';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,27 @@ import { ArticleComponent } from './article/article.component';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  articles: Article[];
 
+  constructor() {
+    this.articles = [
+      new Article('Angular', 'https://angular.dev', 3),
+      new Article('Fullstack', 'https://fullstack.io', 2),
+      new Article('Angular Home', 'https://angular.io', 1)
+    ];
+  }
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    this.articles = [
+      ...this.articles,
+      new Article(title.value, link.value, 0),
+    ];
+    title.value = '';
+    link.value = '';
+    return false;
+  }
+
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
+  }
 }
--- angular-reddit/src/app/article/article.component.ts
+++ angular-reddit/src/app/article/article.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Article } from './article.model';
 
 @Component({
   selector: 'app-article',
@@ -8,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrl: './article.component.css'
 })
 export class ArticleComponent {
+  @Input() article!: Article;
 
+  voteUp(): boolean {
+    this.article.voteUp();
+    return false;
+  }
+
+  voteDown(): boolean {
+    this.article.voteDown();
+    return false;
+  }
 }
