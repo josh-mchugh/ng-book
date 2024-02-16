e98dc64b17c393de1e7ed05cd9db5f48fc557ae9
Josh McHugh <joshua.mchugh01@gmail.com>
2024-02-16 17:34:53 -0500

    Added Product model and ProductComponent within a new @for syntax

--- inventory-app/src/app/app.component.ts
+++ inventory-app/src/app/app.component.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
+import { Product } from './product/product.model';
+import { ProductComponent } from './product/product.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [ProductComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  products: Product[];
+
+  constructor() {
+    this.products = [
+      new Product(
+        'MYSHOES',
+        'Black Running Shoes',
+        '/assests/images/products/black-shoes.jpg',
+        ['Men', 'Shoes', 'Running Shoes'],
+        109.99
+      ),
+      new Product(
+        'NEATOJACKET',
+        'Blue Jacket',
+        '/assets/images/products/blue-jacket.jpg',
+        ['Women', 'Apparel', 'Jackets & Vests'],
+        238.99
+      ),
+      new Product(
+        'NICEHAT',
+        'A Nice Black Hat',
+        '/assets/images/products/black-hat.jpg',
+        ['Men', 'Accessories', 'Hats'],
+        29.99
+      )
+    ];
+  }
+
+  onProductSelected(product: Product): void {
+    console.log('Product clicked: ', product);
+  }
 }
